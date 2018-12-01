fabb69b2c454ce7671d98a036fddf852f87e8d27
Akshay Kumar <akshayk17j@gmail.com>
2018-11-30 20:11:57 -0500

    fixed issue with duplicates

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <tuple>
+#include <cstdlib>
+
+#define Btuple tuple<BNode*,int,CODE>
 
 using namespace std;
 
-enum CODE{NOT_FOUND, SUCESS, DUPLICATE, OVERFLOW};
+enum CODE{NOT_FOUND, SUCCESS, DUPLICATE, OVERFLOW};
 
+template<int M = 5>
 struct BNode{
     int countL;
     int countK;
@@ -14,26 +18,43 @@ struct BNode{
 
 
 // TEST THIS FUNCTION FOR ALL CASES BEFORE YOU MOVE ON!!!// TEST THIS FUNCTION FOR ALL CASES BEFORE YOU MOVE ON!!!
-    /*tuple<BNode*, int, CODE> add(const int val){
-        int index = countL - 1;
-        while(index >= 0 && val <= keys[index]){
-            if(val == keys[index])
-                return tuple<BNode*,int,CODE>(nullptr, val, CODE::DUPLICATE);
-            keys[index + 1] = keys[index]; // draw this out on paper
-            // also need to copy over the links
-            index -= 1;
+    Btuple add(const int &val){
+        if(!countK){
+            keys[0] = val;
+            ++countK;
+            return Btuple(nullptr, val, CODE::SUCCESS);
         }
-        keys[index[]]
+        else{
+            for(int i = 0; i < countK; ++i){
+                if(keys[i] == val)
+                    return Btuple(nullptr, val, CODE::DUPLICATE);
+            } // checks for duplicates
+
+            int index = countK;
+            while(index > 0 && keys[index-1] > val){
+                keys[index] = keys[index - 1];
+                --index;
+            } // shifts over all the values that are smaller
+
+            keys[index] = val;
+            ++countK;
+
+            if(countK == M) // if overflow happened
+                return Btuple(nullptr, val, CODE::OVERFLOW);
+            return Btuple(nullptr, val, CODE::SUCCESS);
+        }
     }
+    
+    /*
     // TEST THIS FUNCTION FOR ALL CASES BEFORE YOU MOVE ON!!!// TEST THIS FUNCTION FOR ALL CASES BEFORE YOU MOVE ON!!!
-    tuple<BNode*, int, CODE> split(){
+    BTuple split(){
 
     }
     // TEST THIS FUNCTION FOR ALL CASES BEFORE YOU MOVE ON!!!// TEST THIS FUNCTION FOR ALL CASES BEFORE YOU MOVE ON!!!
-    tuple<BNode*, int, CODE> merge(){
+    BTuple merge(){
 
     }*/
-    // TEST THIS FUNCTION FOR ALL CASES BEFORE YOU MOVE ON!!!
+
     void printNode(){
         for(int i = 0; i < countK; ++i){
             cout << "link: " << (links[i]? "valid" : "nullptr") << endl;
@@ -44,19 +65,19 @@ struct BNode{
     BNode(){
         countK = 0;
         countL = 0;
-        keys = new int[4];
-        links = new int*[5];
-        for(int i = 0; i < 5; ++i)
+        keys = new int[M];
+        links = new int*[M+1];
+        for(int i = 0; i <= M; ++i)
              links[i] = nullptr;
     }
     
     BNode(int val){
         countK = 1;
         countL = 2;
-        keys = new int[4];
+        keys = new int[M];
         keys[0] = val;
-        links = new int*[5];
-        for(int i = 0; i < 5; ++i)
+        links = new int*[M+1];
+        for(int i = 0; i <= M; ++i)
             links[i] = nullptr;
     }
     ~BNode(){
@@ -66,14 +87,26 @@ struct BNode{
 };
 
 int main(){
-    BNode one(15);
-    BNode two(20);
-    BNode three;
+    
+    cout << endl;
+    
 
-    one.printNode();
-    cout << endl;
-    two.printNode();
-    cout << endl;
-    three.printNode();
+    /* FOR CHECKING IF ADD FUNCTION WORKS
+
+    srand(time(NULL));
+    int val = rand()%10 + 1;
+    for(int i = 0; i < 10; ++i){
+        BNode<10> temp;
+        CODE answer;
+        while((answer = get<2>(temp.add(val))) != CODE::OVERFLOW){
+            val = rand()%10 + 1;
+            if(answer == CODE::DUPLICATE)
+                cout << "DUPLICATE"  << endl;
+        }
+        temp.printNode();
+        cout << endl;        
+    }
+    */
+
     return 0;
 }
